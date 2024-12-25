90f5cfa0a99d573347c46fb266705ac1746d7952
ramji <ramjib2311@gmail.com>
2024-12-25 08:52:03 +0530

    Boom-errro

--- backend/error.js
+++ backend/error.js
@@ -4,12 +4,15 @@ import cors from "cors";
 import mongoose from 'mongoose';
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import Boom from "@hapi/boom";
 
 const app = express();
 const port = 1400;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+
 
 const jwt_key = "s3cur3_K3y!@#2024_very_long_and_random_string";
 
@@ -19,13 +22,13 @@ app.get("/", (req, res) => {
 
 mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.post("/check", async (req, res) => {
+app.post("/check", async (req, res, next) => {
     try {
         const u = req.body.username;
         const pas = req.body.password;
 
         if (!u) {
-            return res.status(401).send("undefined username");
+            throw Boom.badRequest("username is required");
         }
         if (!pas) {
             return res.status(401).send("undefined password");
@@ -48,10 +51,19 @@ app.post("/check", async (req, res) => {
         console.log("Data hashed and inserted");
         res.send("User registered. Send a request for a bearer token.");
     } catch (error) {
-        console.error("Error:", error);
-        res.status(500).send("Internal server error");
+        
+        next(error);
     }
 });
+
+app.use((err,req,res,next)=>{
+    if(err.isBoom){
+        return res.status(err.output.statusCode).json(err.output.payload);
+    }else{
+        res.sendStatus(500);
+    }
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
