ea358c484b364cfcff1a05c62438166a81d059f8
ramji <ramjib2311@gmail.com>
2024-12-27 06:17:11 +0530

    module-error

--- backend/ins.js
+++ backend/ins.js
@@ -3,7 +3,7 @@ const session = new inspector.Session();
 session.connect();
 session.post('Profiler.enable');
 session.post('Profiler.start');
-// Run code
+
 session.post('Profiler.stop', (err, { profile }) => {
   console.log(profile);
 });
